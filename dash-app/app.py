1cbf228d1369bb02521fac3fa3ee4f5bd7c8b899
ayaaurora25 <ayyya.aurora@gmail.com>
2019-12-22 23:01:55 +0700

    decorate interface v1

--- dash-app/app.py
+++ dash-app/app.py
@@ -25,23 +25,6 @@ dataset_bubble['total_respondents'] = total_respondents
 dataset_bubble['color'] = colors
 dataset_bubble['importance'] = dataset_bubble['importance'].apply(lambda x: x*10)
 
-# np.random.seed(999)
-# dataset = pd.DataFrame({
-#   "Sex": np.random.randint(0, 2, 1000),
-#   "Years": np.random.randint(1970, 1980, 1000),
-#   "Age": np.random.randint(18, 65, 1000),
-#   "v1": np.random.randint(0, 2, 1000),
-#   "v2": np.random.randint(0, 2, 1000),
-#   "v3": np.random.randint(0, 2, 1000),
-#   "Category": np.random.randint(0, 8, 1000),
-#   "Percentage": np.random.randint(1, 10, 1000)
-# })
-
-# group_scatter = pd.DataFrame({'count':dataset.groupby(['Age', 'Percentage', 'Category']).size()}).reset_index()
-# group_scatter['count'] = group_scatter['count'].apply(lambda x: x*10)
-
-# df = px.data.gapminder()
-
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -49,112 +32,115 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(
     className="container-fluid",
     children=[
-        html.H1(children='Visualisasi Data Kementrian Kesehatan'),
-
         html.Div(
-            className="row",
+            className="container-fluid",
             children=[
-                html.Div(
-                    className="col-sm-10",
-                    children=[
-                        dcc.Graph(id='bar-graph1'),
-                        html.Div(
-                            className="btn-group d-flex",
-                            children=[
-                                html.Button('Jenis Kelamin', className="btn btn-light", id='sex-button'),
-                                html.Button('Faktor Resiko', className="btn btn-light", id='risk-factor-button'),
-                                html.Button('Umur', className="btn btn-light", id='age-button'),
-                                html.Button('Suku Ayah', className="btn btn-light", id='father-tribe-button'),
-                                html.Button('Suku Ibu', className="btn btn-light", id='mother-tribe-button')
-                            ]
-                        ),
-                    ]
-                ),
-                html.Div(
-                    className="col-sm-2",
-                    children=[
-                        html.Div(
-                            className="form-check",
-                            children=[
-                                html.H5(
-                                    children="Tahapan Survey"
-                                ),
-                                dcc.Checklist(
-                                    className="form-check-label",
-                                    id='var-checkboxes',
-                                    options=[
-                                        {'label': ' 2011', 'value': '2011'},
-                                        {'label': ' 2012', 'value': '2012'},
-                                        {'label': ' 2015', 'value': '2015'}
-                                    ],
-                                    value=['2011', '2012', '2015'],
-                                    labelStyle={'display': 'block'}
-                                )
-                            ]
-                        )
-                        
-                    ]
+                html.H1(
+                    children='Visualisasi Data Kementrian Kesehatan'
                 )
-                
             ]
         ),
 
-        # html.Div(
-        #     className="row",
-        #     children=[
-        #         html.Div(
-        #             className="col-sm-8",
-        #             children=[
-        #                 dcc.Graph(id='dummy-graph2')
-        #             ]
-        #         ),
-        #         html.Div(
-        #             className="col-sm-2",
-        #             children=[
-        #                 dcc.Dropdown(
-        #                     id = 'multi-options',
-        #                     options = [
-        #                         {'label': 'HIV', 'value': 0},
-        #                         {'label': 'Smoking', 'value': 1},
-        #                         {'label': 'Fart', 'value': 2},
-        #                         {'label': 'Stress', 'value': 3},
-        #                         {'label': 'Diet', 'value': 4},
-        #                         {'label': 'Cancer', 'value': 5},
-        #                         {'label': 'Tea', 'value': 6},
-        #                         {'label': 'Coffee', 'value': 7}
-        #                     ],
-        #                     value = [0, 1],
-        #                     multi = True
-        #                 )
-        #             ]
-        #         )
-        #     ]
-        # ),
-
-    
-    
-
-    dcc.Graph(
-        id='dummy-graph3',
-        figure={
-            'data' : [
-                go.Scatter(
-                    x = dataset_bubble['total_respondents'],
-                    y = dataset_bubble['stroke'],
-                    text = dataset_bubble['jenis'],
-                    mode = 'markers',
-                    marker = {
-                        'color' : dataset_bubble['color'],
-                        'size'  : dataset_bubble['importance']
-                    }
+        html.Div(
+            className="container-fluid",
+            children=[
+                html.Div(
+                    className="card",
+                    children=[
+                        html.Div(
+                            className="card-header",
+                        ),
+                        html.Div(   
+                            className="card-body row",
+                            children=[
+                                html.Div(
+                                    className="col-sm-10",
+                                    children=[
+                                        dcc.Graph(id='bar-graph1'),
+                                        html.Div(
+                                            className="btn-group d-flex",
+                                            children=[
+                                                html.Button('Jenis Kelamin', className="border border-secondary btn btn-light", id='sex-button'),
+                                                html.Button('Faktor Resiko', className="border border-secondary btn btn-light", id='risk-factor-button'),
+                                                html.Button('Umur', className="border border-secondary btn btn-light", id='age-button'),
+                                                html.Button('Suku Ayah', className="border border-secondary btn btn-light", id='father-tribe-button'),
+                                                html.Button('Suku Ibu', className="border border-secondary btn btn-light", id='mother-tribe-button')
+                                            ]
+                                        ),
+                                    ]
+                                ),
+                                html.Div(
+                                    className="col-sm-2",
+                                    children=[
+                                        html.Br(),
+                                        html.Div(
+                                            className="form-check",
+                                            children=[
+                                                html.H5(
+                                                    children="Tahapan Survey"
+                                                ),
+                                                dcc.Checklist(
+                                                    className="form-check-label",
+                                                    id='var-checkboxes',
+                                                    options=[
+                                                        {'label': ' 2011', 'value': '2011'},
+                                                        {'label': ' 2012', 'value': '2012'},
+                                                        {'label': ' 2015', 'value': '2015'}
+                                                    ],
+                                                    value=['2011', '2012', '2015'],
+                                                    labelStyle={'display': 'block'}
+                                                )
+                                            ]
+                                        )
+                                     ]
+                                )
+                            ]
+                        )
+                    ]
                 )
-            ],
-            'layout' : dict(
-                title = "Jenis Faktor Resiko vs Jumlah Penderita Stroke vs Besar Resiko",
-                xaxis = {'title' : 'Jumlah Responden'},
-                yaxis = {'title' : 'Jumlah Responden dengan Stroke'}
-            )
-        }
+            ]
+        ),
+        html.Br(),
+        html.Div(
+            className="container-fluid",
+            children=[
+                html.Div(
+                className="card",
+                children=[
+                    html.Div(
+                        className="card-header"
+                    ),
+                    html.Div(
+                        className="card-body",
+                        children=[
+                            dcc.Graph(
+                                id='scatter-graph3',
+                                figure={
+                                    'data' : [
+                                        go.Scatter(
+                                            x = dataset_bubble['total_respondents'],
+                                            y = dataset_bubble['stroke'],
+                                            text = dataset_bubble['jenis'],
+                                            mode = 'markers',
+                                            marker = {
+                                                'color' : dataset_bubble['color'],
+                                                'size'  : dataset_bubble['importance']
+                                            }
+                                        )
+                                    ],
+                                    'layout' : dict(
+                                        title = "Jenis Faktor Resiko vs Jumlah Penderita Stroke vs Besar Resiko",
+                                        xaxis = {'title' : 'Jumlah Responden'},
+                                        yaxis = {'title' : 'Jumlah Responden dengan Stroke'}
+                                    )
+                                }
+                            )
+                        ]
+                    )
+                ]
+            ),
+        html.Br()
+        ]
     )
 ])
 
@@ -168,7 +154,7 @@ app.layout = html.Div(
     Input(component_id='mother-tribe-button', component_property='n_clicks_timestamp')],
 )
 
-def update_figure1(selected_var, sex_clicked, risk_clicked, age_clicked, father_tribe_clicked, mother_tribe_clicked):
+def update_figure(selected_var, sex_clicked, risk_clicked, age_clicked, father_tribe_clicked, mother_tribe_clicked):
     buttons = [0, 0, 0, 0, 0]
     traces = []
     col = "umur"
@@ -222,44 +208,12 @@ def update_figure1(selected_var, sex_clicked, risk_clicked, age_clicked, father_
     
     return {
         'data' : traces,
-        'layout' : {
-            'title': 'Frekuensi Tahap Survey vs ' + title_feature,
-            'barmode': 'stack'
-        }
+        'layout' : dict(
+            title= 'Jumlah Responden vs ' + title_feature,
+            barmode= 'stack',
+            yaxis={'title':'Jumlah Responden'}
+        )
     }
-
-# @app.callback(
-#     Output(component_id='dummy-graph2', component_property='figure'),
-#     [Input(component_id='multi-options', component_property='value')]
-# )
-# def update_figure2(selected_opt):
-#     traces = []
-
-#     grouped_sum_1 = dataset.groupby(['Category']).sum()['v1']
-#     list_sum_1 = grouped_sum_1.values.tolist()
-#     grouped_sum_2 = dataset.groupby(['Category']).sum()['v2']
-#     list_sum_2 = grouped_sum_2.values.tolist()
-#     grouped_sum_3 = dataset.groupby(['Category']).sum()['v3']
-#     list_sum_3 = grouped_sum_3.values.tolist()
-
-#     y_opt = []
-#     for opt in selected_opt:
-#         y = []
-#         y.append(list_sum_1[opt])
-#         y.append(list_sum_2[opt])
-#         y.append(list_sum_3[opt])
-#         y_opt.append(y)
-
-#     for i in range(len(selected_opt)):
-#         traces.append(go.Bar(name=str(selected_opt[i]), x=y_opt[i], y=['v1', 'v2', 'v3'], orientation='h'))
-    
-#     return {
-#         'data' : traces,
-#         'layout' : {
-#             'title': 'Dummy Data Visualization',
-#             'barmode': 'stack'
-#         }
-#     }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
